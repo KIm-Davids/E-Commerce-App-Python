8b6894ed069a6855c221d55093b76b66ab835cd6
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-10-24 20:24:34 +0100

    COnnected to mysql database

--- demo_project/settings.py
+++ demo_project/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'demo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'demo_project',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost'
     }
 }
 
