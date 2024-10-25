0eb20d7e81a1c42ecc211e8f19e2a66f9461fc24
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-10-25 10:47:56 +0100

    Added new classes for the project

--- store/models.py
+++ store/models.py
@@ -3,7 +3,6 @@ from django.conf import settings
 
 
 # Create your models here.
-<<<<<<< HEAD
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
@@ -11,15 +10,10 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255, null=False, blank=False)
-=======
-class Product(models.Model):
-    title = models.CharField(max_length =255, null=False, blank=False)
->>>>>>> fddd4f46c97da1ff325237857d7b4dd48b7c1931
     description = models.CharField(max_length=255, null=False, blank=False)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.PositiveIntegerField()
     last_updated = models.DateTimeField(auto_now=True)
-<<<<<<< HEAD
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotion = models.ManyToManyField('Promotion', related_name='+')
 
@@ -39,7 +33,6 @@ class Order(models.Model):
         ('P', 'Pending'),
         ('S', 'Success'),
         ('F', 'Failed')
-
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS, default='P')
@@ -64,16 +57,3 @@ class Address(models.Model):
 class Promotion(models.Model):
     promotion = models.ManyToManyField(Product, related_name='+')
     discount = models.DecimalField(max_digits=6, decimal_places=2)
-=======
-
-class Collection(models.Model):
-    title = models.CharField(max_length=255)
-
-class Cart(models.Model):
-    title = models.CharField(max_length=255)
-
-class CartItem(models.Model):
-    cart =
-    product =
-    quantity =
->>>>>>> fddd4f46c97da1ff325237857d7b4dd48b7c1931
