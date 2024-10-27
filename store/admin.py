a3034a10ced12d8e03279947555fbeb21febbf58
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-10-27 13:20:26 +0100

    Added more methods and connected admin, try to mix the module issue

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
-
-# Register your models here.
+# from django.contrib import admin
+# import models
+#
+#
+# # Register your models here.
+# @admin.register(models.Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     ...
+#
+#
+# @admin.register(models.Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = {'title', 'price', 'description'}
